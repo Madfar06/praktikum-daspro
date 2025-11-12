3e9abf960de06ac6f0ee566d9405c8fe1c938761
Madfar06 <gfrnrl19@gmail.com>
2025-11-13 02:04:11 +0700

    Modifikasi percobaan 3

--- arrayRataNilai15.java
+++ arrayRataNilai15.java
@@ -1,23 +1,41 @@
-
 import java.util.Scanner;
 
+// Saya memperbaiki typo 'blic' menjadi 'public'
 public class arrayRataNilai15 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+        
+        int[] nilaiMhs = new int[jumlahMhs];
+        double totalLulus = 0;
+        int jumlahLulus = 0; 
+        double totalTidakLulus = 0;
+        int jumlahTidakLulus = 0; 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.println("Masukan nilai mahasiswa ke- " + (i+1) + " :");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("rata-rata nilai =" + rata2);
+
+        double rataLulus = (jumlahLulus > 0) ? (totalLulus / jumlahLulus) : 0;
+        double rataTidakLulus = (jumlahTidakLulus > 0) ? (totalTidakLulus / jumlahTidakLulus) : 0;
+
+        
+        System.out.println("Banyaknya mahasiswa yang lulus: " + jumlahLulus);
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        
+        sc.close(); 
     }
 }
